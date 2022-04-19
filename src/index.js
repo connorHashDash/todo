2563040d3d6fb5fc4c664448fb87eb5a4cef4b7b
connorHashDash <ckb1997@hotmail.co.uk>
2022-04-19 21:54:47 +0100

    basic styling of todo cards

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import './style.css'
 import {populateSideBar} from './leftbar.js'
 import {populateTopBar} from './topbar.js'
 import {tabMaker} from './topbar.js'
+import {toDoItem} from './todoCards.js'
 
 tabMaker('hello')
 tabMaker('goodbye')
 
+toDoItem('testToDo', `
+hello you must do things, heellooooooo
+`, '12/12/22')
+
+toDoItem('testToDo2', `
+hello you must do things, heellooooooo hey hello hi lol adfasdfa
+`, '01/02/22')
 
 let todooey = (() => {
   populateSideBar()
--- src/todoCards.js
+++ src/todoCards.js
@@ -1,7 +1,49 @@
 import {elementFactory} from './ElementMaker.js'
 
-let toDoItem {
-  elementFactory('div', {
+let display = document.getElementById('display')
+
+let toDoItem = (name, desc, dueDate) => {
+  let toDoHolder = elementFactory('div', {
     className: 'toDoItem'
   })
+
+
+  let nameDiv = elementFactory('div', {
+    className: 'nameDiv'
+  })
+
+  let toDoName = elementFactory('p', {
+    className: 'toDoName',
+    innerHTML: name,
+  })
+
+  let descDiv= elementFactory('div', {
+    className: 'descDiv'
+  })
+
+  let toDoDesc = elementFactory('p', {
+    className: 'toDoDesc',
+    innerHTML: desc,
+  })
+
+  let dueDateDiv = elementFactory('div', {
+    className: 'dueDateDiv'
+  })
+
+  let toDoDueDate = elementFactory('p', {
+    className: 'toDoDueDate',
+    innerHTML: dueDate,
+  })
+
+  nameDiv.appendChild(toDoName)
+  descDiv.appendChild(toDoDesc)
+  dueDateDiv.appendChild(toDoDueDate)
+
+  toDoHolder.appendChild(nameDiv)
+  toDoHolder.appendChild(descDiv)
+  toDoHolder.appendChild(dueDateDiv)
+
+  display.appendChild(toDoHolder)
 }
+
+export {toDoItem}
